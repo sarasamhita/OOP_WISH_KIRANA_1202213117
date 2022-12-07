293168d2a9c0cb4641658131750f00b6f96d071a
sarasamhita <91931605+sarasamhita@users.noreply.github.com>
2022-12-07 16:14:17 +0700

    Add files via upload

---
+++ MODUL2_KIRANA/src/MainApp.java
@@ -0,0 +1,8 @@
+public class MainApp {
+    public static void main(String[] args) {
+        TransportasiAir transportasi = new TransportasiAir(4, 20000);
+        transportasi.informasi();
+        System.out.println(" ");
+
+    }
+}
---
+++ MODUL2_KIRANA/src/Sampan.java
@@ -0,0 +1,25 @@
+public class Sampan extends TransportasiAir {
+    protected int layar;
+
+    public Sampan(int jumlahKursi, int biaya, int layar){
+        super(jumlahKursi, biaya);
+        this.layar = layar;
+    }
+
+    @Override
+    public void informasi(){
+        System.out.println("Transportasi Air jenis Sampan dengan kursi berjumlah " + jumlahKursi + " ditetapkan dengan biaya sebesar Rp. " + biaya);
+    }
+
+    public void berlayar(){
+        System.out.println("Transportasi Air jenis Sampan sedang berlajar menggunakan " + layar + " layar");
+    }
+
+    public void berlabuh(){
+        System.out.println("Transportasi Air jenis Sampan berlabuh di pantai ");
+    }
+
+    public void berlabuh(int jangkar){
+        System.out.println("Transportasi Air jenis Sampan berlabuh di pantai menggunakan " + jangkar + " Jangkar");
+    }
+}
